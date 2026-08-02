b2b90b25b528ac0fcd7b4482a5468d5aae6cc110
binny <binny0x00@gmail.com>
2026-08-02 14:17:43 +0900

    feat: Contact CTA와 푸터 구현

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,15 +6,18 @@ useSeoMeta({
 </script>
 
 <template>
-  <div
-    class="mx-auto flex w-full max-w-[720px] flex-col gap-16 desktop:grid desktop:max-w-none desktop:grid-cols-[470px_minmax(0,720px)] desktop:items-start desktop:gap-[74px]"
-  >
-    <HeroSection />
-    <div class="flex min-w-0 flex-col gap-[104px]">
-      <AboutSection />
-      <ExperienceSection />
-      <ProjectsSection />
-      <WritingSection />
+  <div class="flex w-full flex-col gap-12 desktop:gap-16">
+    <div
+      class="mx-auto flex w-full max-w-[720px] flex-col gap-16 desktop:grid desktop:max-w-none desktop:grid-cols-[470px_minmax(0,720px)] desktop:items-start desktop:gap-[74px]"
+    >
+      <HeroSection />
+      <div class="flex min-w-0 flex-col gap-[104px]">
+        <AboutSection />
+        <ExperienceSection />
+        <ProjectsSection />
+        <WritingSection />
+      </div>
     </div>
+    <ContactFooter />
   </div>
 </template>
